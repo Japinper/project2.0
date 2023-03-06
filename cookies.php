5e3f00d195515946f424bcff42f0a6764319640d
Japinper <77630407+Japinper@users.noreply.github.com>
2023-03-06 17:44:04 +0100

    Update cookies.php

--- cookies.php
+++ cookies.php
@@ -1,5 +1,5 @@
 <?php
 $value = 'true';
 setcookie("Cookie", $value, time()+3600);  /* expira en 1 hora */
-echo '<meta http-equiv="refresh" content="0; url=http://192.168.204.120">';
+echo '<meta http-equiv="refresh" content="0; url=http://54.80.212.77">';
 ?>
